5a1dca6635dc85d92a34953fc9d00fbf44878f8f
Nate Spilman <natespilman@Nates-MacBook-Pro.local>
2019-05-06 17:08:40 -0700

    majorly stepping on file sizes

---
+++ images.py
@@ -0,0 +1,24 @@
+from PIL import Image
+from resizeimage import resizeimage
+import os
+
+def resizeImage(filepath,factor):
+    try:
+        image_raw = open(filepath,'r+b')
+        image = Image.open(image_raw)
+        width,height = image.size
+        if ignoreTest(filepath) == False:
+            resized_image = resizeimage.resize_width(image,width * factor)
+            resized_image.save(filepath,image.format)
+    except:
+        pass
+
+root = './images/photos_from_dan/eventPhotos/'
+
+def ignoreTest(string):
+    return "ignore" in string
+
+
+photosFromDan = os.listdir(root)
+# print(photosFromDan)
+[resizeImage(root + photo,.35) for photo in photosFromDan]
